e5a35c097572e1ebb8ccd3f39517c8d70c6f4182
Scott.yang 楊慶堂 <scott.yang@cohesiondata.com>
2021-08-10 08:36:15 +0800

    加上awesome-hammerspoon 的桌簿及月曆

--- ime.lua
+++ ime.lua
@@ -1,8 +1,12 @@
 local function Chinese()
+    hs.console.printStyledtext("chinese")
+    hs.console.printStyledtext(hs.keycodes.currentSourceID())
     hs.keycodes.currentSourceID("im.rime.inputmethod.Squirrel.Rime")
 end
 
 local function English()
+    hs.console.printStyledtext("English")
+    hs.console.printStyledtext(hs.keycodes.currentSourceID())
     hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
 end
 
@@ -10,17 +14,18 @@ local function set_app_input_method(app_name, set_input_method_function, event)
     event = event or hs.window.filter.windowFocused
     hs.window.filter.new(app_name)
       :subscribe(event, function() set_input_method_function() end)
-  end
-  set_app_input_method('Hammerspoon', English, hs.window.filter.windowCreated)
-  set_app_input_method('Spotlight', English, hs.window.filter.windowCreated)
-  set_app_input_method('Terminal', English)
-  set_app_input_method('iTerm2', English)
-  set_app_input_method('Microsoft Edge', English)
-  set_app_input_method('Google Chrome', English)
-  set_app_input_method('Safari', English)
-  set_app_input_method('Code', English)
-  set_app_input_method('AnyDesk', English)
-  set_app_input_method('Spring Tool Suite 4', English)
-  set_app_input_method('LINE', Chinese)
-  set_app_input_method('Telegram', Chinese)
-  -----------------------mine end-----------------------------
+end
+set_app_input_method('Hammerspoon', English, hs.window.filter.windowCreated)
+set_app_input_method('Spotlight', English, hs.window.filter.windowCreated)
+set_app_input_method('Terminal', English)
+set_app_input_method('iTerm2', English)
+set_app_input_method('Microsoft Edge', English)
+set_app_input_method('Google Chrome', English)
+set_app_input_method('Safari', English)
+set_app_input_method('Code', English)
+set_app_input_method('AnyDesk', English)
+set_app_input_method('DBeaver', English)
+set_app_input_method('Spring Tool Suite 4', English)
+set_app_input_method('LINE', Chinese)
+set_app_input_method('Telegram', Chinese)
+-----------------------mine end-----------------------------
--- init.lua
+++ init.lua
@@ -3,4 +3,6 @@ require "reload"
 require "usb"
 require "line"
 
-
+hs.loadSpoon("CircleClock")
+hs.loadSpoon("BingDaily")
+hs.loadSpoon("HCalendar")
