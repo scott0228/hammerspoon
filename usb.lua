b14ce74137a857bac0157c30ed4a0cb3ceb7600f
Scott.yang 楊慶堂 <scott.yang@cohesiondata.com>
2024-01-16 14:17:34 +0800

    改用 log

--- usb.lua
+++ usb.lua
@@ -1,7 +1,8 @@
+local log = hs.logger.new('usb.lua', 'debug')
+
 function usbDeviceCallback(data)
-    print("usbDeviceCallback: "..hs.inspect(data))
+    log.d("usbDeviceCallback: "..hs.inspect(data))
 end
   
 usbWatcher = hs.usb.watcher.new(usbDeviceCallback)
 usbWatcher:start()
-  
